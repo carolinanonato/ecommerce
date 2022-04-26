07dbde7ac2ece2114f69de75e8f941f491715312
Carolina Nonato <csnonato@gmail.com>
2022-04-26 16:16:45 -0400

    increase and decreasing items

---
+++ context/StateContext.js
@@ -0,0 +1,45 @@
+import React, { createElement, useContext, useState, useEffect } from "react";
+import { toast } from 'react-hot-toast'
+import { createContext } from "react/cjs/react.production.min";
+
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+
+    const [showCart, setShowCart] = useState(false);
+    const [cartItems, showCartItems] = useState();
+    const [totalPrice, setTotalPrice] = useState();
+    const [totalQuantities, settotalQuantities] = useState();
+    const [qty, setQty] = useState(1);
+
+    const incQty = () => {
+        setQty((prevQty) => prevQty + 1)
+    }
+
+
+    const decQty = () => {
+        setQty((prevQty) => {
+            if (prevQty - 1 < 1) return 1
+            return prevQty - 1
+        })
+    }
+
+
+    return (
+        <Context.Provider value={{
+            showCart,
+            cartItems,
+            totalPrice,
+            totalQuantities,
+            qty,
+            incQty,
+            decQty
+        }}>
+            {children}
+        </Context.Provider>
+    )
+
+
+}
+
+export const useStateContext = () => useContext(Context)
