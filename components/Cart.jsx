d7a71123c78d462074ee3c672d83471413c4cda6
Carolina Nonato <csnonato@gmail.com>
2022-04-27 18:15:21 -0400

    update cart bug with item orders

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -10,7 +10,7 @@ import {urlFor} from '../lib/client'
 function Cart() {
 
   const cartRef = useRef()
-  const {totalPrice, totalQuantities, cartItems, setShowCart} = useStateContext()
+  const {totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity} = useStateContext()
 
   return (
     <div className='cart-wrapper' ref={cartRef}>
@@ -45,9 +45,9 @@ function Cart() {
                   <div className="flex bottom">
                     <div>
                     <p className="quantity-desc">
-                            <span className="minus" onClick=""><AiOutlineMinus /></span>
-                            <span className="num">0</span>
-                            <span className="plus" onClick=""><AiOutlinePlus /></span>
+                            <span className="minus" onClick={() => toggleCartItemQuantity(item._id,'dec')}><AiOutlineMinus /></span>
+                            <span className="num">{item.quantity}</span>
+                            <span className="plus" onClick={() => toggleCartItemQuantity(item._id,'inc')}><AiOutlinePlus /></span>
                         </p>
                     </div>
                     <button className="remove-item" type="button" onClick=""><TiDeleteOutline/></button>
